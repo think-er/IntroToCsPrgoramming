cc895a02c2dca094e02b9a9ec2b535d40e673843
thinker <75255732+think-er@users.noreply.github.com>
2022-09-29 05:52:11 +0900

    Delete 02-Language_structure directory

--- 02-Language_structure/2-01 IdentifierApp.cs
+++
@@ -1,9 +0,0 @@
-﻿//using System;
-//class IdentifierApp {
-//    public static void Main() {
-//        int MyVar = 1, myvar = 2;
-//        int @int = 10, 변수 = 20;
-//        Console.WriteLine("MyVar = " + MyVar + ", myvar = " + myvar);
-//        Console.WriteLine("@int = " + @int + ", 변수 = " + 변수);
-//    }
-//}
