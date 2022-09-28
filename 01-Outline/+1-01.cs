c6aaa6f728e5cd3a303019c2a617dfd5bfae2122
thinker <jeonghun5092@gmail.com>
2022-09-29 00:50:06 +0900

    접근 지정자

---
+++ 01-Outline/+1-01.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+
+//class CoffeeMaker {
+//    public bool onState;
+//    public void StartCoffeeMaker() {
+//        if (onState == true)
+//            Console.WriteLine("The CoffeeMaker is already on");
+//        else
+//            onState = true;
+//        Console.WriteLine("The CoffeeMaker is now on");
+//    }
+//    public static void Main() {
+//        CoffeeMaker cm = new CoffeeMaker();
+//        cm.onState = true;
+//        cm.StartCoffeeMaker();
+//    }
+//}
