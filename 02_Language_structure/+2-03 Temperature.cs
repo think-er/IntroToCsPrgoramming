366965340c82c756c2c5bf804d5efbfc1c27e231
thinker <jeonghun5092@gmail.com>
2022-09-30 10:54:52 +0900

    .

--- 02_Language_structure/+2-03 Temperature.cs
+++ 02_Language_structure/+2-03 Temperature.cs
@@ -1,11 +1,11 @@
-﻿using System;
-enum Temperature { Low = 2, Medium, High };
-// enum의 순서값 변경 2, 3, 4
-class Temp {
-    public static void Main() {
-        Temperature value = Temperature.Medium;
-        int val = (int)value;
-        // 열거형을 정수형으로 casting(형변환)
-        Console.WriteLine("Temperature value is.." + val);
-    }
-}
+﻿//using System;
+//enum Temperature { Low = 2, Medium, High };
+//// enum의 순서값 변경 2, 3, 4
+//class Temp {
+//    public static void Main() {
+//        Temperature value = Temperature.Medium;
+//        int val = (int)value;
+//        // 열거형을 정수형으로 casting(형변환)
+//        Console.WriteLine("Temperature value is.." + val);
+//    }
+//}
---
+++ 02_Language_structure/2-10 ArrayTypeApp.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//class ArrayTypeApp {
+//    public static void Main() {
+//        int[] ia = new int[3];
+//        int[] ib = { 1, 2, 3 };
+//        // int[] ib = new int[] {1, 2, 3} 같은 의미이다.
+//        int i;
+//        for (i=0; i < ia.Length; i++)
+//            ia[i] = ib[i];
+//        for (i = 0; i < ia.Length; i++)
+//            Console.WriteLine(ia[i] + " ");
+//        Console.WriteLine();
+//    }
+//}
